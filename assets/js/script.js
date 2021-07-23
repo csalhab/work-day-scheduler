e0a448c110400c8edc1332ae9b582f975153a6fb
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-07-23 00:37:19 -0400

    added click handler for all save buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,13 +16,21 @@ var hourBlocks = ["10am", "11am", "12pm", "1pm", "2pm", "3pm","4pm", "5pm"];
 
 var divContainerEl = $("#container");
 
+/*
+var userEntry = {
+    hour: hour.value,
+    description: description.value.trim(),
+    date: date.value
+};
+*/
+
 //FUNCTIONS =================================================
-// [CREATE]
 
 $.each(hourBlocks, function(i, hourBlock) {
-    // Creates a new "<div>" with its children elements:
+    //Creates a new "<div>" with its children elements:
     //"<div>" "<textarea>" and <button>
     //for each hourBlock
+    //Builds & Places them all too
     var divTimeblockEl = $("<div></div>");
     //divTimeblockEl.text(hourBlock);
     divTimeblockEl.attr("class", "time-block row");
@@ -37,16 +45,37 @@ $.each(hourBlocks, function(i, hourBlock) {
     divTimeblockEl.append(textAreaEl);
 
     var buttonEl = $("<button></button>");
+    var idCounter = i + 2;
     buttonEl.text("save");
-    buttonEl.attr("class", "saveBtn btn col col-lg-1");
+    buttonEl.attr({
+        class: "saveBtn btn col col-lg-1",
+        id: "saveBtn" + idCounter
+    });
     divTimeblockEl.append(buttonEl);
 
     divContainerEl.append(divTimeblockEl);
  });
 
+function storeEntry() {
+    //to be done
+}
 
 
 //USER INTERACTIONS =========================================
+//gets and listens for all save buttons being clicked and 
+//is able to identify each by their unique id
+for (var i = 1; i < hourBlocks.length+2; i++) {
+    var saveBtnClicked = "#saveBtn" + i;
+    var saveBtnEl = "saveBtnEl" + i;
+    var saveBtnEl = $(saveBtnClicked);
+
+    saveBtnEl.on("click", function (event) {
+        event.preventDefault();
+        console.log("save button " + event.target.id + " clicked");
+        //call storeEntry()
+    });
+
+}
 
 
 //INITIALIZATION ============================================
