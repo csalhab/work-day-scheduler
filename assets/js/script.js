ef32437f2e33f369ee5a89101f912a6b8835d658
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-07-23 20:32:33 -0400

    figured out the moment hour and getting hour that's for the button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,27 @@
 //[CREATE] retrieve p tag id=#currentDay
 var currentDayEl = $("#currentDay");
 
-//[BUILD] call moment, format as example, store into variable now
+//[BUILD] call moment:
+var now = moment();
+
+//format now as example, store into variable schedulerTopDate
 //example: Thursday, July 22nd
-var now = moment().format("dddd, MMMM Do");
+var schedulerTopDate = now.format("dddd, MMMM Do");
+
+//gets current hour from momentjs now variable
+//returns in military 24 hr time
+var hour = now.hour();
+console.log(hour);
+//convert/format to standard time 
+//var standardHour = moment(hour, "H").format("h");
+
+var hourNoPeriod;
 
 //DATA ======================================================
 
 //[PLACE] put the date as text onto the proper p tag
-currentDayEl.text(now);
+currentDayEl.text(schedulerTopDate);
+
 
 var hourBlocks = ["10am", "11am", "12pm", "1pm", "2pm", "3pm","4pm", "5pm"];
 
@@ -54,6 +67,7 @@ $.each(hourBlocks, function(i, hourBlock) {
     divTimeblockEl.append(buttonEl);
 
     divContainerEl.append(divTimeblockEl);
+
  });
 
 function storeEntry() {
@@ -72,7 +86,32 @@ for (var i = 1; i < hourBlocks.length+2; i++) {
     saveBtnEl.on("click", function (event) {
         event.preventDefault();
         console.log("save button " + event.target.id + " clicked");
+        //console.log(event.currentTarget.parentNode.children[0].childNodes[0].textContent);
+        var hourPeriodFromBtn = event.currentTarget.parentNode.children[0].childNodes[0].textContent;
+        var hourPeriodFromBtnLength = event.currentTarget.parentNode.children[0].childNodes[0].textContent.length;
+        console.log(hourPeriodFromBtn);
         //call storeEntry()
+
+        //get just hour; leave off am/pm periods & cast to Number
+        //hourNoPeriod = Number(event.target.text.substring(0, event.target.text.length - 2));
+        hourNoPeriod = Number(hourPeriodFromBtn.substring(0, hourPeriodFromBtnLength - 2));
+        console.log("hourNoPeriod: " + hourNoPeriod);
+        //only need afternoon biz hours 1, 2, 3, 4, 5
+        if (hourNoPeriod < 12 && hourNoPeriod !== 9 && hourNoPeriod !== 10 && hourNoPeriod !== 11) {
+            //convert only afternoon biz hours to military time
+            //will be used to compare with momentjs current hour
+            hourNoPeriod = hourNoPeriod + 12;
+        };
+        console.log(hourNoPeriod);
+        if (hour === hourNoPeriod) {
+            console.log("present");
+        };
+        if (hour > hourNoPeriod) {
+            console.log("past");
+        } else {
+            console.log("future");
+        };
+
     });
 
 }
